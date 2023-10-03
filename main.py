c9f77081b837faefce407043472265529d078ddf
pc <njmtdeen@gmail.com>
2023-10-03 23:07:52 +0200

    second commit

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(HelpApame):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {HelpApame}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
